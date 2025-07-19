ed95e0f14cc1c5375f54f0b0918f24b19643fb82
vzaporozky <zaporozhskiy2002@gmail.com>
2025-07-19 12:19:44 +0300

    BalanceManager

--- blockchain/contracts/BalanceManager.sol
+++ blockchain/contracts/BalanceManager.sol
@@ -8,48 +8,32 @@ contract BalanceManager is Ownable, ReentrancyGuard {
 	mapping(address => uint256) private balances;
 
 	event Deposit(address indexed user, uint256 amount);
-	event Withdrawal(address indexed user, uint256 amount);
-	event Transfer(address indexed from, address indexed to, uint256 amount);
+	event BalanceDecreased(address indexed user, uint256 amount);
 
 	constructor() Ownable(msg.sender) {}
 
-	// Deposit ETH to the contract
+	function getBalance(address user) external view returns (uint256) {
+		return balances[user];
+	}
+
 	function deposit() external payable nonReentrant {
 		require(msg.value > 0, 'Deposit amount must be greater than 0');
 		balances[msg.sender] += msg.value;
 		emit Deposit(msg.sender, msg.value);
 	}
 
-	// Withdraw ETH from the contract
-	function withdraw(uint256 amount) external nonReentrant {
-		require(amount > 0, 'Withdrawal amount must be greater than 0');
-		require(balances[msg.sender] >= amount, 'Insufficient balance');
+	function decreaseBalance(address user) external onlyOwner nonReentrant {
+		require(user != address(0), 'Invalid user address');
 
-		balances[msg.sender] -= amount;
-		(bool success, ) = msg.sender.call{ value: amount }('');
-		require(success, 'Withdrawal failed');
+		uint256 decrease = 1 ether;
+		require(decrease > 0, 'Amount must be greater than 0');
+		require(balances[user] >= decrease, 'Insufficient balance');
 
-		emit Withdrawal(msg.sender, amount);
+		balances[user] -= decrease;
+
+		emit BalanceDecreased(user, decrease);
 	}
 
-	// Transfer balance to another user
-	function transfer(address to, uint256 amount) external nonReentrant {
-		require(to != address(0), 'Invalid recipient');
-		require(amount > 0, 'Transfer amount must be greater than 0');
-		require(balances[msg.sender] >= amount, 'Insufficient balance');
-
-		balances[msg.sender] -= amount;
-		balances[to] += amount;
-
-		emit Transfer(msg.sender, to, amount);
-	}
-
-	// Get balance of an address
-	function getBalance(address user) external view returns (uint256) {
-		return balances[user];
-	}
-
-	// Allow owner to withdraw contract balance (emergency use)
 	function emergencyWithdraw() external onlyOwner nonReentrant {
 		uint256 contractBalance = address(this).balance;
 		require(contractBalance > 0, 'No funds to withdraw');
--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -7,11 +7,17 @@ async function main() {
 	// await NFTMarket.waitForDeployment();
 	// console.log('NFTMarket deployed to:', await NFTMarket.getAddress());
 
-	const Test = await ethers.getContractFactory('Test');
-	console.log('Deploying Test...');
-	const test = await Test.deploy('Hello world');
-	await test.waitForDeployment();
-	console.log('test deployed to:', await test.getAddress());
+	const BalanceManager = await ethers.getContractFactory('BalanceManager');
+	console.log('Deploying BalanceManager...');
+	const bm = await BalanceManager.deploy();
+	await bm.waitForDeployment();
+	console.log('BalanceManager deployed to:', await bm.getAddress());
+
+	// const Test = await ethers.getContractFactory('Test');
+	// console.log('Deploying Test...');
+	// const test = await Test.deploy('Hello world');
+	// await test.waitForDeployment();
+	// console.log('test deployed to:', await test.getAddress());
 }
 
 main()
