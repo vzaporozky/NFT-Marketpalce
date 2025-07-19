14c00bde2c7c32d504080e20e8f1bcfbb2f97408
vzaporozky <zaporozhskiy2002@gmail.com>
2025-07-19 12:34:37 +0300

    test BalanceManager contract in backend

--- backend/src/modules/photo/controllers/photoController.ts
+++ backend/src/modules/photo/controllers/photoController.ts
@@ -1,19 +1,21 @@
+import { balanceManagerEther } from '../../../utils/balanceMaganer';
 import { createPhotoService } from '../services/photoService';
 
 export const createPhoto = async (request, reply) => {
 	try {
 		const { title, description, userAddress } = request.body;
 
-		const { photoId, photoPath } = await createPhotoService(
-			title,
-			description,
-			userAddress
-		);
+		// const { photoId, photoPath } = await createPhotoService(
+		// 	title,
+		// 	description,
+		// 	userAddress
+		// );
 
-		// return reply.code(201).send({ photo });
+		// testEther();
+		balanceManagerEther();
+
 		reply.header('Content-Type', 'image/jpeg');
-
-		return reply.sendFile(photoId + '.png');
+		// return reply.sendFile(photoId + '.png');
 	} catch (error) {
 		return reply.code(500).send({ error: 'Failed to create or send photo' });
 	}
