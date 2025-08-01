74118b7c4fbab6f9218af690a9d094f919d0afb0
vzaporozky <zaporozhskiy2002@gmail.com>
2025-08-01 14:11:31 +0300

    add createPhoto func. to contract

--- frontend/packages/hardhat/contracts/BalanceManager.sol
+++ frontend/packages/hardhat/contracts/BalanceManager.sol
@@ -1,44 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract BalanceManager is Ownable, ReentrancyGuard {
-	mapping(address => uint256) private balances;
+    mapping(address => uint256) private balances;
+    uint256 price = 0.0008 ether;
 
-	event Deposit(address indexed user, uint256 amount);
-	event BalanceDecreased(address indexed user, uint256 amount);
+    event Deposit(address indexed user, uint256 amount);
+    event BalanceDecreased(address indexed user, uint256 amount);
 
-	constructor() Ownable(msg.sender) {}
+    constructor() Ownable(msg.sender) {}
 
-	function getBalance(address user) external view returns (uint256) {
-		return balances[user];
-	}
+    function getBalance(address user) external view returns (uint256) {
+        return balances[user];
+    }
 
-	function deposit() external payable nonReentrant {
-		require(msg.value > 0, 'Deposit amount must be greater than 0');
-		balances[msg.sender] += msg.value;
-		emit Deposit(msg.sender, msg.value);
-	}
+    function deposit() external payable nonReentrant {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
 
-	function decreaseBalance(address user) external onlyOwner nonReentrant {
-		require(user != address(0), 'Invalid user address');
+    function createPhoto() external nonReentrant {
+        require(balances[msg.sender] >= price, "Insufficient balance");
+        balances[msg.sender] -= price;
+        emit BalanceDecreased(msg.sender, price);
+    }
 
-		uint256 decrease = 1 ether;
-		require(decrease > 0, 'Amount must be greater than 0');
-		require(balances[user] >= decrease, 'Insufficient balance');
+    function emergencyWithdraw() external onlyOwner nonReentrant {
+        uint256 contractBalance = address(this).balance;
+        require(contractBalance > 0, "No funds to withdraw");
 
-		balances[user] -= decrease;
-
-		emit BalanceDecreased(user, decrease);
-	}
-
-	function emergencyWithdraw() external onlyOwner nonReentrant {
-		uint256 contractBalance = address(this).balance;
-		require(contractBalance > 0, 'No funds to withdraw');
-
-		(bool success, ) = owner().call{ value: contractBalance }('');
-		require(success, 'Emergency withdrawal failed');
-	}
+        (bool success, ) = owner().call{ value: contractBalance }("");
+        require(success, "Emergency withdrawal failed");
+    }
 }
